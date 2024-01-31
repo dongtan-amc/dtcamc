3a57a7a0b05bcaf6049eafbf0d62df3bf687b306
howooking <junsgk@gmail.com>
2024-01-31 18:46:20 +0900

    refactor : generateStaticParams

--- app/community/news/[slug]/page.tsx
+++ app/community/news/[slug]/page.tsx
@@ -1,4 +1,13 @@
+import { getPostsMetadata } from "@/lib/utils";
 import Post from "../../post";
+
+export function generateStaticParams() {
+  const posts = getPostsMetadata("markdown/news/");
+
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+}
 
 export default function SingleNewsPage({
   params: { slug },
--- app/community/notice/[slug]/page.tsx
+++ app/community/notice/[slug]/page.tsx
@@ -1,4 +1,13 @@
+import { getPostsMetadata } from "@/lib/utils";
 import Post from "../../post";
+
+export function generateStaticParams() {
+  const posts = getPostsMetadata("markdown/notice/");
+
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+}
 
 export default function SingleNoticePage({
   params: { slug },
