9bf0b23d32c91d563313fe6dbae2b153341da94f
howooking <junsgk@gmail.com>
2024-04-07 01:31:58 +0900

    feat : 랜딩 carousel 기본 설정

--- components/sections/landing/landing-carousel.tsx
+++ components/sections/landing/landing-carousel.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Container from "@/components/common/container";
+import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
@@ -10,12 +11,13 @@ import {
   type CarouselApi,
 } from "@/components/ui/carousel";
 import { HERO_IMAGES } from "@/constants/hero-images";
+import { cn } from "@/lib/utils";
 import Autoplay from "embla-carousel-autoplay";
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 
 export default function LandingCarousel() {
-  const plugin = useRef(Autoplay({ delay: 3000, stopOnInteraction: true }));
+  const plugin = useRef(Autoplay({ delay: 4000, stopOnInteraction: false }));
 
   const [api, setApi] = useState<CarouselApi>();
   const [current, setCurrent] = useState(0);
@@ -35,28 +37,60 @@ export default function LandingCarousel() {
   }, [api]);
 
   return (
-    <Carousel
-      opts={{
-        align: "start",
-        loop: true,
-      }}
-      setApi={setApi}
-      plugins={[plugin.current]}
-      className="w-full"
-      onMouseEnter={plugin.current.stop}
-      onMouseLeave={plugin.current.reset}
-    >
-      <CarouselContent>
-        {HERO_IMAGES.map((image, index) => (
-          <Container key={index}>
-            <CarouselItem className="basis-1">
-              <Image src={image.src} alt="landing image" />
+    <div className=" flex justify-end">
+      <Carousel
+        setApi={setApi}
+        opts={{ loop: true }}
+        plugins={[plugin.current]}
+        className="w-full h-[70vh] overflow-hidden flex items-center"
+      >
+        <CarouselContent>
+          {HERO_IMAGES.map((image) => (
+            <CarouselItem key={image.title}>
+              <div className="relative">
+                <div className="bg-gradient-to-r from-black/70 via-black/50 to-black/10 absolute inset-0" />
+                <div className="absolute top-1/3 left-[10%] text-white">
+                  <p>Dongtan City Animal Medical Center</p>
+                  <p className="text-3xl">{image.title}</p>
+                  <p>{image.subTitle}</p>
+                </div>
+                <Image
+                  src={image.src}
+                  alt="landing images"
+                  className="w-full"
+                />
+              </div>
             </CarouselItem>
-          </Container>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+          ))}
+        </CarouselContent>
+        <div className="absolute top-[10%] right-[10%]">
+          <CarouselPrevious className="bg-transparent rounded-md text-white w-10 h-10" />
+          <CarouselNext className="bg-transparent rounded-md text-white w-10 h-10" />
+        </div>
+        <div className="absolute bottom-[5%] left-[calc(50%-40px)] text-white flex gap-4">
+          {Array.from({ length: count }).map((element, index) => (
+            <div
+              key={index}
+              className={cn(
+                "w-2 h-2 rounded-full bg-white ring-0",
+                current === index + 1 && "ring-primary ring-2 bg-transparent"
+              )}
+            />
+          ))}
+        </div>
+      </Carousel>
+    </div>
+
+    // <CarouselContent>
+    //   {HERO_IMAGES.map((image) => (
+    //     <Container key={image.title}>
+    //       <CarouselItem className="w-full">
+    //         <Image src={image.src} alt="landing image" />
+    //       </CarouselItem>
+    //     </Container>
+    //   ))}
+    // </CarouselContent>
+    // <CarouselPrevious />
+    // <CarouselNext />
   );
 }
--- constants/hero-images.ts
+++ constants/hero-images.ts
@@ -1,34 +1,34 @@
 import recepetion from "@/public/photo/reception.jpeg";
 import treatment from "@/public/photo/treatment-room.jpeg";
-import operation from "@/public/photo/operation-room.jpeg";
+import icu from "@/public/photo/icu1.jpeg";
 import consulting from "@/public/photo/consulting-room2.jpeg";
 
 export const HERO_IMAGES = [
   {
     src: recepetion,
     title:
-      "언제나 반려동물의 건강과 함께 보호자의 행복한 삶을 위해 노력하겠습니다",
+      "언제나 반려동물의 건강과 함께 보호자의 행복한 삶을 위해 노력하겠습니다1",
     subTitle:
       "숙련된 수의사와 의료진과 함께 언제나 든든한 동물병원이 되겠습니다",
   },
   {
     src: treatment,
     title:
-      "언제나 반려동물의 건강과 함께 보호자의 행복한 삶을 위해 노력하겠습니다",
+      "언제나 반려동물의 건강과 함께 보호자의 행복한 삶을 위해 노력하겠습니다2",
     subTitle:
       "숙련된 수의사와 의료진과 함께 언제나 든든한 동물병원이 되겠습니다",
   },
   {
-    src: operation,
+    src: icu,
     title:
-      "언제나 반려동물의 건강과 함께 보호자의 행복한 삶을 위해 노력하겠습니다",
+      "언제나 반려동물의 건강과 함께 보호자의 행복한 삶을 위해 노력하겠습니다3",
     subTitle:
       "숙련된 수의사와 의료진과 함께 언제나 든든한 동물병원이 되겠습니다",
   },
   {
     src: consulting,
     title:
-      "언제나 반려동물의 건강과 함께 보호자의 행복한 삶을 위해 노력하겠습니다",
+      "언제나 반려동물의 건강과 함께 보호자의 행복한 삶을 위해 노력하겠습니다4",
     subTitle:
       "숙련된 수의사와 의료진과 함께 언제나 든든한 동물병원이 되겠습니다",
   },
