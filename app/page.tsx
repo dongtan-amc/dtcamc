2129f9fd1d48ec324a1b6475ef7ed2c505887ca2
howooking <junsgk@gmail.com>
2024-04-28 10:20:13 +0900

    fix : 빌드 에러

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import FacilitySection from "@/components/sections/facility/facility-section";
 import Landing from "@/components/sections/landing/landing";
 import MapSection from "@/components/sections/map/map-section";
 import MoreSection from "@/components/sections/more/more-section";
-import SubjectSection from "@/components/sections/subject(x)/subject-section";
 
 export default function Home() {
   return (
