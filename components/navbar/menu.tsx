3f629e92e5ab34eaca67c1298298c77a1442e08c
Jungwoo Lee <87072568+howooking@users.noreply.github.com>
2024-08-07 16:14:35 +0900

    배포

--- components/navbar/menu.tsx
+++ components/navbar/menu.tsx
@@ -1,26 +1,28 @@
 import { KAKAO } from "@/constants/general-info";
 import { SECTIONS } from "@/constants/sections";
 import Link from "next/link";
+import { RiKakaoTalkFill } from "react-icons/ri";
 
 export default function Menu() {
   return (
-    <ul className="grid-cols-5 gap-1.5 items-center justify-center xl:grid hidden ">
+    <ul className="hidden grid-cols-5 items-center justify-center gap-1.5 xl:grid">
       {SECTIONS.map((section) => (
         <li key={section.sectionTitle} className="text-center">
           <Link
             href={`/${section.hash}`}
-            className="text-white text-lg px-4 hover:text-olive-drab-400 transition"
+            className="px-4 text-lg text-white transition hover:text-olive-drab-400"
           >
             {section.sectionTitle}
           </Link>
         </li>
       ))}
-      <li className="w-full flex justify-center">
+      <li className="flex w-full justify-center">
         <Link
           href={KAKAO}
           target="_blank"
-          className="text-[#191919] bg-[#FEE500] hover:bg-[#FEE500] hover:opacity-70 w-[80%] text-balance text-center rounded-md py-1"
+          className="flex w-[80%] items-center justify-center gap-1 text-balance rounded-md bg-[#FEE500] py-1 text-center text-[#191919] hover:bg-[#FEE500] hover:opacity-70"
         >
+          <RiKakaoTalkFill className="h-6 w-6" />
           카카오톡
         </Link>
       </li>
