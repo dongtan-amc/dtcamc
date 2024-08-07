34409ef815b449f40f7bd72703c8f38eb97ea44c
Jungwoo Lee <87072568+howooking@users.noreply.github.com>
2024-08-07 10:50:34 +0900

    any

--- components/sections/landing/react-youtube.tsx
+++ components/sections/landing/react-youtube.tsx
@@ -1,14 +1,19 @@
 "use client";
 
+import useBreakpoint from "@/hooks/useBreakPoint";
+import useWindowSize from "@/hooks/useWindowSize";
 import YouTube, { YouTubeProps } from "react-youtube";
 
 export default function ReactYoutube() {
+  const { height, width } = useWindowSize();
+  const currentScreenSize = useBreakpoint();
+
   const opts: YouTubeProps["opts"] = {
-    width: "100%",
-    height: "100%",
+    width: "200",
+    height: "100",
     playerVars: {
       rel: 0,
-      autoplay: 1,
+      autoplay: 0,
       modestbranding: 1,
       loop: 1,
       playlist: "BlzbRNfgcVU",
@@ -17,12 +22,12 @@ export default function ReactYoutube() {
   return (
     <YouTube
       videoId="BlzbRNfgcVU"
-      onReady={(e) => {
-        e.target.mute(); //소리 끔
-        e.target.playVideo();
+      onReady={(e: any) => {
+        e.target.mute();
+        // e.target.playVideo();
       }}
       opts={opts}
-      iframeClassName="relative w-full h-auto aspect-video"
+      iframeClassName="relative w-full h-full h-screen ring aspect-video"
     />
   );
 }
