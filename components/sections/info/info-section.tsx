4df7c3c9c1444d408d48e9fbc5871848b2ba4113
sgwanlee <seonggwan.lee@gmail.com>
2025-06-05 07:46:16 +0900

    진료시간 변경

--- components/sections/info/info-section.tsx
+++ components/sections/info/info-section.tsx
@@ -4,9 +4,9 @@ import Container from "@/components/common/container";
 import DividerBottom from "@/components/common/divider-bottom";
 import {
   DAY_TIME,
+  LUNCH_TIME,
   NIGHT_TIME,
   PHONE,
-  SUNDAY_TIME,
 } from "@/constants/general-info";
 import { FaClock } from "react-icons/fa";
 import { FaPhone } from "react-icons/fa6";
@@ -37,13 +37,10 @@ const INFOS = [
     title: "진료시간",
     description: (
       <div>
+        <p>365일 24시간 연중무휴</p>
         <p>주간 : {DAY_TIME}</p>
         <p>야간 : {NIGHT_TIME}</p>
-        <p>일요일 : {SUNDAY_TIME}</p>
-        <p className="pt-[35px] text-base">
-          ※ 야간 진료 시간 이후에는 <br />
-          입원환자 관리에 집중하기 위해 <br /> 진료를 제한합니다.
-        </p>
+        <p>점심시간 : {LUNCH_TIME}</p>
       </div>
     ),
     icon: FaClock,
--- constants/general-info.ts
+++ constants/general-info.ts
@@ -12,9 +12,6 @@ export const KAKAO = "https://pf.kakao.com/_RLdib";
 export const KAKAO_MAP =
   "https://map.kakao.com/?urlX=513717&urlY=1030622&urlLevel=3&itemId=1809300423&q=24%EC%8B%9C%EB%8F%99%ED%83%84%EC%8B%9C%ED%8B%B0%EB%8F%99%EB%AC%BC%EC%9D%98%EB%A3%8C%EC%84%BC%ED%84%B0&srcid=1809300423&map_type=TYPE_MAP";
 export const DAY_TIME = "오전 9시 - 오후 9시";
-export const NIGHT_TIME = "오후 9시 - 오전 12시";
-export const SUNDAY_TIME = "오전 9시 - 오후 7시";
-export const LATE_NIGHT_TIME =
-  "※ 야간에는 입원 환자 관리에 집중하기 위해 야간 진료 시간 이후에는 진료를 제한합니다.";
+export const NIGHT_TIME = "오후 9시 - 오전 9시";
 export const LUNCH_TIME = "오후 12시 - 오후 1시";
 export const MODOO = "https://dtctamc.modoo.at/?link=4iwunk1g";
