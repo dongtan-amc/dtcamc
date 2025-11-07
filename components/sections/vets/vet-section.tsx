ef463d278d025fd4cf251245d3ed96a19045d9ff
dongtan-amc <dongtanvet24@gmail.com>
2025-11-07 11:17:05 +0900

    수의사 조현민,황태하 삭제, 장영옥 추가

--- components/sections/vets/vet-section.tsx
+++ components/sections/vets/vet-section.tsx
@@ -1,12 +1,12 @@
 import Container from "@/components/common/container";
 import vet1 from "@/public/vets/vet1_bg_removed.png";
 import vet2 from "@/public/vets/vet2_bg_removed.png";
-import vet3 from "@/public/vets/vet3_bg_removed.png";
+// import vet3 from "@/public/vets/vet3_bg_removed.png";
 // import vet4 from "@/public/vets/vet4_bg_removed.png"; 지워
 // import vet7 from "@/public/vets/vet7_bg_removed.png";
 import vet8 from "@/public/vets/vet8_bg_removed.png";
 import vet9 from "@/public/vets/vet_gojongchan.png";
-import vet10 from "@/public/vets/vet_hwangth.png";
+import vet10 from "@/public/vets/vet_jangyoungok.png";
 import SectionLayout from "../section-layout";
 import SectionTitle from "../section-title";
 import VetArticle from "./vet-article";
@@ -43,33 +43,6 @@ const VETS = [
       "Advanced Surgical Techniques by Dr. F.A. (Tony) Mann, Advanced ECG Interpretation:by Dr. Teresa DeFrancesco (IVECCS, San Antonio, TX, USA)",
       "DFO/PTO/TPLO-M/TTTT2 by Massimo Petazzoni (Dubai, UAE)",
       "Advanced TPLO/CBLO by Kenneth Bruecker & Donald Hulse (VITICUS, Las Vegas, NV, USA",
-    ],
-  },
-  {
-    name: "조현민",
-    title: "외과과장",
-    subjects: ["정형외과(Orthopedic Surgery)", "치과(Dentistry)"],
-    image: vet3,
-    history: [
-      "충북대 수의과대학 학사 졸업",
-      "전남대 수의외과학 박사 졸업",
-      "전남대 외과임상조교",
-      "전남대 동물병원 외과 팀장",
-      "24시동탄시티동물의료센터 외과과장",
-      "",
-      "",
-      "한국임상수의학회 정회원",
-      "한국수의치과협회 정회원",
-    ],
-    expert: [
-      "Innoplant Veterinary Total Hip Replacement-cementless and cemented implantation (2023)",
-      "Veterinary Dental Forum (2024)",
-      "Application of modified porcine xenograft by collagen coating in the veterinary field: Pre-clinical and clinical evaluations (Frontiers in Veterinary Science, 2024)",
-      "Successful Surgical Treatment of Severe Lower Lip Avulsion in Two Cats (Journal of Veterinary Clinics, 2022)",
-      "Laparoscopic attenuation of a congenital extrahepatic portosystemic shunt in a dog—a thin-film banding for splenophrenic shunt: A case report (Frontiers in Veterinary Science, 2022)",
-      "Effects of a Topically Applied Oral Wound Dressing Film on Intra-oral Wound Healing in Rabbits (In Vivo, 2022)",
-      "Guided Bone Regeneration in Comminuted Long-Bone Fractures Using Recombinant Human Bone Morphogenetic Protein-2 and a Collagen Membrane (Journal of Veterinary Clinics, 2022)",
-      "Effect of quercetin on formation of porcine neutrophil extracellular trap (Veterinary Immunology and Immunopathology, 2021)",
     ],
   },
   {
@@ -136,18 +109,17 @@ const VETS = [
     ],
   },
  {
-     name: "황태하",
+     name: "장영욱",
      title: "응급과장",
      subjects: ["응급진료(Emergency & Critical Care"],
      image: vet10,
      history: [
-       "경북대 수의과대학 졸업",
-       "전)N 동물병원 진료수의사",
-       "전)L 동물병원 내과 원장",
-       "24시동탄시티동물의료센터 응급과장",
-      "",
-      "",
-      "한국동물병원협회 KAHA 정회원",
+       "전남대 수의과대학 졸업",
+       "전)전남대학동물병원 임상조교",
+       "전)M동물병원 진료수의사",
+       "전)G동물메디컬센터 응급수의사",
+      "전)B동물병원 응급수의사",
+      "24시동탄시티동물의료센터 응급과장",
     ],
     expert: [],
    },
